b6cc7a574772ae71fda7a9a845423ca6eeec6d69
jokerKwu <pkjhj485@naver.com>
2020-08-21 10:41:39 +0900

    채팅방 나가기 버튼 생성

--- src/main/java/com/joker/webchatting/springboot/web/ChatController.java
+++ src/main/java/com/joker/webchatting/springboot/web/ChatController.java
@@ -1,17 +1,21 @@
 package com.joker.webchatting.springboot.web;
 
+import com.joker.webchatting.springboot.config.auth.LoginUser;
+import com.joker.webchatting.springboot.config.auth.dto.SessionUser;
 import com.joker.webchatting.springboot.domain.chat.ChatMessage;
 import com.joker.webchatting.springboot.domain.chat.ChatRequest;
 import com.joker.webchatting.springboot.domain.chat.ChatResponse;
 import com.joker.webchatting.springboot.domain.chat.MessageType;
 import com.joker.webchatting.springboot.service.chat.ChatService;
 import com.joker.webchatting.springboot.util.ServletUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @RestController
 public class ChatController {
 
@@ -73,4 +78,6 @@ public class ChatController {
         }
     }
     // -- tag :: websocket stomp
+
+
 }
