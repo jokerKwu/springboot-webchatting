06b20d2c1db42cffaefb78a59198ca9438bb9925
jokerKwu <pkjhj485@naver.com>
2020-09-03 17:24:06 +0900

    IndexController 페이지번호 String으로 변환4

--- src/main/java/com/joker/webchatting/springboot/web/IndexController.java
+++ src/main/java/com/joker/webchatting/springboot/web/IndexController.java
@@ -40,7 +40,7 @@ public class IndexController {
         System.out.println("페이지에 표시할 엘리먼트 수    : "+ postList.getSize());
         System.out.println("현재 페이지 인덱스   : "+ postList.getNumber());
         int[] pageNumber = new int[100];
-        for (int i=0;i<postList.getTotalPages();i++){
+        for (int i=0;i<2;i++){
             pageNumber[i] = i+1;
         }
         model.addAttribute("pageNumber",pageNumber);
