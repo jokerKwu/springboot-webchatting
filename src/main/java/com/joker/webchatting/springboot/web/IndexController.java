fdd0912311b1ce1ad04f1676631ab9ff0e0e38bd
jokerKwu <pkjhj485@naver.com>
2020-09-04 13:15:59 +0900

    IndexController 페이지번호 mustache 문법 사용

--- src/main/java/com/joker/webchatting/springboot/web/IndexController.java
+++ src/main/java/com/joker/webchatting/springboot/web/IndexController.java
@@ -42,10 +42,8 @@ public class IndexController {
         System.out.println("전체 페이지 수   : "+ postList.getTotalPages());
         System.out.println("페이지에 표시할 엘리먼트 수    : "+ postList.getSize());
         System.out.println("현재 페이지 인덱스   : "+ postList.getNumber());
-        Set<Integer> arr = new HashSet<Integer>();
-        arr.add(1);
-        arr.add(2);
-        model.addAttribute("pageNumber",arr);
+
+        model.addAttribute("pageNumber",postList.getTotalPages());
 
         if (user != null) {
             model.addAttribute("name",user.getName());
