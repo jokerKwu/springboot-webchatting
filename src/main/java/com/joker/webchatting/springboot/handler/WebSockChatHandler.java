b4b32c72ef3d0e0abee440a24578e0a1d251c346
jokerKwu <pkjhj485@naver.com>
2020-09-07 15:26:45 +0900

    웹 채팅 웹소켓 테스트

--- src/main/java/com/joker/webchatting/springboot/handler/WebSockChatHandler.java
+++ src/main/java/com/joker/webchatting/springboot/handler/WebSockChatHandler.java
@@ -1,14 +1,17 @@
 package com.joker.webchatting.springboot.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Primary
 @Slf4j
 @Component
 public class WebSockChatHandler extends TextWebSocketHandler {
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
         String payload = message.getPayload();
