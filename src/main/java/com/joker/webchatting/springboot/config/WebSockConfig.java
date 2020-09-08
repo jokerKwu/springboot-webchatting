cd29628fe668c69dfd9f1e448db7511328033679
jokerKwu <pkjhj485@naver.com>
2020-09-08 20:54:53 +0900

    vue.js 코드 수정5dd3

--- src/main/java/com/joker/webchatting/springboot/config/WebSockConfig.java
+++ src/main/java/com/joker/webchatting/springboot/config/WebSockConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
@@ -18,8 +19,9 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
     }
 
 }
