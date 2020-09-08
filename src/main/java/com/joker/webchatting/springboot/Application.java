132010b1ce515b0086c2d150afe173f69c915839
jokerKwu <pkjhj485@naver.com>
2020-09-08 11:19:55 +0900

    chat 자바빈 충돌 오류 수정2

--- src/main/java/com/joker/webchatting/springboot/Application.java
+++ src/main/java/com/joker/webchatting/springboot/Application.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled","false");
-
-        System.setProperty("spring.devtools.livereload.enabled","true");
         SpringApplication.run(Application.class, args);
 
     }
