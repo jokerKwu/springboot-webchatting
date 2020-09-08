44968ee7f746143db3b30e768d6d4830c2a5ad3b
jokerKwu <pkjhj485@naver.com>
2020-09-08 14:03:50 +0900

    Stomp로 채팅서버 고도화하기

--- src/main/java/com/joker/webchatting/springboot/domain/chat/ChatRoom.java
+++ src/main/java/com/joker/webchatting/springboot/domain/chat/ChatRoom.java
@@ -1,13 +1,7 @@
 package com.joker.webchatting.springboot.domain.chat;
-
-import com.joker.webchatting.springboot.service.chat.ChatService;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Getter
--- src/main/java/com/joker/webchatting/springboot/web/ChatController.java
+++ src/main/java/com/joker/webchatting/springboot/web/ChatController.java
@@ -1,28 +1,10 @@
 package com.joker.webchatting.springboot.web;
 
-import com.joker.webchatting.springboot.config.auth.LoginUser;
-import com.joker.webchatting.springboot.config.auth.dto.SessionUser;
 import com.joker.webchatting.springboot.domain.chat.*;
-import com.joker.webchatting.springboot.service.chat.ChatService;
-import com.joker.webchatting.springboot.util.ServletUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.async.DeferredResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
-
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
